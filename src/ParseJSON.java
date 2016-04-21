0d26786c86c28a5ef0612ec51ac98ff50d73a42c
Eleton <eleton@gmail.com>
2016-04-21 13:57:13 +0200

    Har jag fått upp det nu eller?

--- src/ParseJSON.java
+++ src/ParseJSON.java
@@ -11,24 +11,27 @@ import java.util.Scanner;
 
 public class ParseJSON {
     public static void main(String[] args) throws FileNotFoundException {
+        //Läser in en specifik JSON-fil och sparar i en File-klass
+        //I framtiden borde file ta en inparameter istället för hårdkodad path
         File file = new File("json/fact.ce.cc.be.f.json");
         FileReader fileRead = new FileReader(file);
         Scanner scan = new Scanner(fileRead);
+        //Läser in JSON-filen till en enda lång sträng
         String jsonString = "";
         while(scan.hasNext()){
             jsonString += scan.nextLine();
         }
-        JSONObject jObj = new JSONObject(jsonString);
-        JSONObject graph = jObj.getJSONObject("op-struct").getJSONObject("graph");
+        JSONObject jsonObject = new JSONObject(jsonString);
+        JSONObject graph = jsonObject.getJSONObject("op-struct").getJSONObject("graph");
         JSONArray edges = graph.getJSONArray("edges");
-        System.out.println(edges.getJSONArray(0));
 
+        //Omvandlar från JSON-formatet till Edge-klassen
         ArrayList<Edge> edgeList = new ArrayList<>();
         for(int i = 0; i < edges.length(); i++){
-            JSONArray JSONedge = edges.getJSONArray(i);
-            int source = JSONedge.getInt(0);
-            int target = JSONedge.getInt(1);
-            String etype = JSONedge.getJSONObject(2).getString("etype");
+            JSONArray jsonEdge = edges.getJSONArray(i);
+            int source = jsonEdge.getInt(0);
+            int target = jsonEdge.getInt(1);
+            String etype = jsonEdge.getJSONObject(2).getString("etype");
             edgeList.add(new Edge(source, target, etype));
         }
 
