62349e81d7d9c3b56a33d1be85b93c99ee75e462
Eleton <eleton@gmail.com>
2016-04-22 14:33:32 +0200

    La till kommentarer till Application

--- src/Application.java
+++ src/Application.java
@@ -10,29 +10,30 @@ import java.io.FileNotFoundException;
 public class Application {
     public static void main(String args[]) throws FileNotFoundException {
 
-        Graph g = ParseJSONf.parse(chooseFFile(args));					// Driver function
+        Graph g = ParseJSONf.parse(chooseFFile(args));					// Driver function      /// Unnecessary comment
         g.display();
 
     }
     public static File chooseFFile(String[] a){
         File file = null;
-        if(a.length > 0){
+        if(a.length > 0){                                               // If there is a cml-argument it will run with that
             file = new File(a[0]);
-        }else{
+        }else{                                                          // If argument list is empty it will let you choose a file with JFileChooser
             final JFileChooser fc = new JFileChooser();
             int returnVal = fc.showOpenDialog(null);
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 file = fc.getSelectedFile();
             }else{
-                System.out.println("You failed to choose a file, n00b...");
+                System.out.println("You failed to choose a file, n00b..."); // This is for those who fail to choose a file
             }
         }
-        if(file == null){
+        if(file == null){                                               // If no file is selected the program will shut down
             System.exit(0);
         }
         return file;
     }
-    public static File choosePFile(String[] a){
+
+    public static File choosePFile(String[] a){                         // Currently unused function that does the same as the one above but with the pattern-JSON-files
         File file = null;
         if(a.length > 1){
             file = new File(a[1]);
