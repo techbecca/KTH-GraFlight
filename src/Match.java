0863dc0a467ac5db3e37f18e1723f63926c1b8f2
my <my>
2016-04-22 15:33:36 +0200

    he

--- src/Match.java
+++ src/Match.java
@@ -1,15 +1,19 @@
+/**
+ * Created by Mathilda on 2016-04-21.
+ * Modified by Mathilda on 2016-04-22. (added MatchId)
+ */
 import java.util.ArrayList;
 
 public class Match {
-	
+
 	final private ArrayList<NodeToNode> NodeToNodeList;
 	final private int MatchId;
-	
-	public Match(ArrayList<NodeToNode> NodeToNodeList) {
+
+	public Match(ArrayList<NodeToNode> NodeToNodeList, int MatchId) {
 
 		this.NodeToNodeList = NodeToNodeList;
 		this.MatchId = MatchId;
-		
+
 	}
 
 
