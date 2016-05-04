a73411e118500cb6b5947ab9890dc3198210482d
Charlotta Spik <spik@kth.se>
2016-05-04 15:49:37 +0200

    Added test class for parseJSONp and added toString to Match

---
+++ src/ParseJSONpTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import org.graphstream.graph.Graph;
+
+
+public class ParseJSONpTest {
+
+	public void parsepTest () throws FileNotFoundException {
+		
+		ParseJSONp p = new ParseJSONp();
+		
+		//Create a new file to use as test file
+		File f = new File("C:/Users/Charlotta/projectX/json/testparsejsonp.json");
+		
+		ArrayList <Match> result = p.parsep(f);
+
+		//assertEquals compares the expected output of the function with the actual output.
+		//If these do not match, an error has occurred. 
+		assertEquals("[Nodes: [5, 15, 43, 59, ], InstructionID: 0, PatternID: 0]", result.toString());
+				
+	}
+	
+}
