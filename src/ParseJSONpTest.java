f049fe0e5b4d24f6ac9e177ea81a594b0c0286e8
Charlotta Spik <spik@kth.se>
2016-05-09 09:50:16 +0200

    Added comments in ParseJSONpTest

--- src/ParseJSONpTest.java
+++ src/ParseJSONpTest.java
@@ -5,12 +5,25 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import org.graphstream.graph.Graph;
+import org.junit.Test;
 
-
+/**
+ * This is a test class for ParseJSONp
+ * @author Mathilda Strandberg von Schantz
+ * @author Charlotta Spik
+ * @version 1.0
+ * @since 2016-05-04
+ */
 public class ParseJSONpTest {
 
+	@Test
+	/**
+	 * This method tests the method "parse" in class "ParseJSOnp".
+	 * @throws FileNotFoundException
+	 */
 	public void parsepTest () throws FileNotFoundException {
 		
+		//creates an instance of the class that we are testing
 		ParseJSONp p = new ParseJSONp();
 		
 		//Create a new file to use as test file
