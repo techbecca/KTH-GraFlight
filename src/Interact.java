45b57df97204857b4d0340939f66d6c6a367a468
Jacob <jacobhul@kth.se>
2016-05-09 16:51:08 +0200

    Initial commit of extracting interactivity from Application.java

---
+++ src/Interact.java
@@ -0,0 +1,12 @@
+import org.graphstream.ui.view.util;
+
+public class Interact {
+
+	public class Keyhandler implements ShortcutManager{
+		public void init();
+	}
+
+	public class Mousehandler implements MouseManager {
+
+	}
+}
