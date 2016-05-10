21b2e48491e648655a1f2e50b89573c5b5af1c24
Jacob <jacobhul@kth.se>
2016-05-10 16:35:47 +0200

    Commented UImod and matchhighlighting

--- src/UImod.java
+++ src/UImod.java
@@ -1,8 +1,19 @@
-import org.graphstream.graph.Node;
+import org.graphstream.graph.Element;
 
+/**
+ * This class conatins methods to modify UI CSS classes of nodes and edges
+ * @author Aristotle
+ * @version 0.01
+ * @since 2016-05-10
+ */
 public class UImod {
-	public static void adduiC(Node node, String attr) {
-		String priors = node.getAttribute("ui.class");
-		if(!priors.contains(attr)) node.setAttribute("ui.class", priors + "," + attr);
+	/**
+	 * This method adds an attribute to the given node
+	 * @param Node a Graphiel node
+	 * @param attr a String that contains the value of to be added to the ui.class key
+	 */
+	public static void adduiC(Element e, String attr) {
+		String priors = e.getAttribute("ui.class");
+		if(!priors.contains(attr)) e.setAttribute("ui.class", priors + "," + attr);
 	}
 }
