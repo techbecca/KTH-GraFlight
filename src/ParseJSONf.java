5c6863c2fe81e98a6a81aa41afe45ce5e56fe423
Rebecca Hellström Karlsson <rhells@kth.se>
2016-05-12 09:12:46 +0200

    Moved convertNode and convertEdge back to ParseJSONf from Graphiel
    Since Graphiel was becoming too crowded with methods, the methods convertEdge were moved to ParseJSONf since that was the only place they were used. They are still public static methods"

--- src/ParseJSONf.java
+++ src/ParseJSONf.java
@@ -21,6 +21,9 @@ public class ParseJSONf {
 	 * @param file This is a JSON file
 	 * @return Returns a graph 
 	 * @throws FileNotFoundException
+     *
+     * Written by Aiman Josefsson 2016-04-26
+     * Modified by Mathilda von Schantz and Rebecca Hellström Karlsson 2016-05-09
 	 */
     public static Graphiel parse(File file) throws FileNotFoundException {
 		
@@ -87,7 +90,7 @@ public class ParseJSONf {
             }
 
             // Set graphical properties to the node
-            Graphiel.convertNode(node);
+            convertNode(node);
 
         }
         
@@ -103,15 +106,15 @@ public class ParseJSONf {
 			edge.setAttribute("etype", etype);
 
             // This will actually be removed later, but it works this way
-            Graphiel.convertEdge(edge);
+            convertEdge(edge);
 
             // Assign that nodes are part of the control flow
             if (etype.equals("ctrl")){
                 gsgraph.getNode(source).setAttribute("ftype", "ctrlFlow");
                 gsgraph.getNode(target).setAttribute("ftype", "ctrlFlow");
 
-                Graphiel.convertNode(gsgraph.getNode(source));
-                Graphiel.convertNode(gsgraph.getNode(target));
+                convertNode(gsgraph.getNode(source));
+                convertNode(gsgraph.getNode(target));
             }
         }
         
@@ -119,11 +122,13 @@ public class ParseJSONf {
     }
 
     /**
-     * This method takes a Graphstream graph and copies the nodes and edges from that graph into a directed JGraph,
-     * which will be used for positioning.
+     * This method takes a Graphstream graph and copies the nodes and edges from
+     * that graph into a directed JGraph,  which will be used for positioning.
      * @param gsgraph is the Graphstream graph with nodes and edges
      * @return a directed graph in the JGraph format: DirectedGraph<String, DefaultEdge>
      * @throws FileNotFoundException
+     *
+     * Written by Aiman Josefsson and Rebecca Hellström Karlsson 2016-04-29
      */
     public static DirectedGraph fromGStoJG(Graph gsgraph) throws FileNotFoundException {
         DirectedGraph<String, DefaultEdge> directedGraph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
@@ -156,5 +161,103 @@ public class ParseJSONf {
         String lol2 = String.valueOf(lol);
         System.out.println(lol2);
 
+    }
+
+    /**
+     * This method iterates through node attributes and parses it into graphical attributes.
+     * @param node
+     *
+     * Written by Aiman Josefsson 2016-04-26
+     * Modified by Rebecca Hellström Karlsson and Mathilda von Schantz 2016-05-09
+     * Modified by Nahida Islam and Mathilda von Schantz 2016-05-10
+     */
+    public static void convertNode(Node node){
+        StringBuilder sb = new StringBuilder();
+        StringBuilder label = new StringBuilder();
+        StringBuilder size = new StringBuilder();
+
+        // Shows node ID as text on the graph
+        String id = node.getAttribute("id");
+        label.append(id + ": ");
+
+        // Continue building string depending on type
+        String ntype = node.getAttribute("ntype");
+
+
+        sb.append(ntype);
+
+        if (ntype.equals("copy")){
+            label.append("cp");
+            size.append("70gu");
+
+        }
+        else  if (ntype.equals("data")){
+            label.append("d");
+            size.append("150gu");
+
+        }
+        else if (ntype.equals("phi")){
+            label.append("phi");
+            size.append("70gu");
+
+        }
+
+
+        if(node.hasAttribute("block-name")){
+            String blockName = node.getAttribute("block-name");
+            sb.append("," + blockName);
+            // Mark the entry node
+            if(node.getAttribute("block-name").equals("entry")){
+                sb.replace(0,sb.length(), "entry");
+                label.replace(0,label.length(), id + ": Entry");
+                size.append("300gu");
+
+            }
+            else{
+                label.append(blockName);
+                size.append("150gu");
+            }
+        }
+
+
+        if(node.hasAttribute("dtype")){
+            String dtype = node.getAttribute("dtype");
+            sb.append("," + dtype);
+        }
+
+        if(node.hasAttribute("op")){
+            String op = node.getAttribute("op");
+            //sb.append("," + op);
+            label.append(op);
+            size.append("75gu");
+
+        }
+
+        if(node.hasAttribute("origin")){
+            String origin = node.getAttribute("origin");
+            //sb.append("," + origin);
+        }
+
+        if(node.hasAttribute("ftype")){
+            String ftype = node.getAttribute("ftype");
+            sb.append("," + ftype);
+        }
+
+        // Set graphical properties to the node
+        node.addAttribute("ui.class", sb.toString());
+        // Set text to be shown on the node
+        node.setAttribute("ui.label", label.toString());
+        node.addAttribute("ui.size", size);
+    }
+
+    /**
+     * This method assigns graphical attributes to an edge based on its edge type.
+     * @param edge
+     *
+     * Written by Aiman Josefsson 2016-04-30
+     */
+    public static void convertEdge(Edge edge){
+        String etype = edge.getAttribute("etype");
+        edge.addAttribute("ui.class", etype);
     }
 }
