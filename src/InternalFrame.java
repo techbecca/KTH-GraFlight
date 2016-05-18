2f595a137e57c75615a9c3bbfdf05a991627927b
Mathilda <millasvs@gmail.com>
2016-05-18 15:47:27 +0200

    now there are also checkboxes but nothing happens in the graph

--- src/InternalFrame.java
+++ src/InternalFrame.java
@@ -1,30 +1,18 @@
-import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 import java.util.List;
 import java.awt.Toolkit;
-
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Enumeration;
 
-import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
+
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JToolBar;
-import javax.swing.SwingConstants;
 
-
-import javax.swing.JTree;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeSelectionModel;
@@ -35,7 +23,6 @@ import javax.swing.event.TreeSelectionListener;
 import org.graphstream.graph.Node;
 import org.graphstream.ui.swingViewer.DefaultView;
 
-import scala.xml.include.sax.Main;
 
 /**
  * This class adds a toolbar to the graph window 
@@ -55,30 +42,28 @@ public class InternalFrame implements TreeSelectionListener {
 	 * @param graph a Grahiel graph
 	 * @param view Graphstream DefaultView from Application
 	 */
-	private JTree tree;
+	private CheckBoxTree tree;
 
 	public void createFrame (JFrame frame, Graphiel graph, DefaultView view){
 
-		//		JButton button = new JButton("Patterns");
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int width = (int) screenSize.getWidth();
 		int height = (int) screenSize.getHeight();
-		
-		
+
+
 		//Create a JFrame for the toolbar
 		JFrame tb = new JFrame ("Toolbar");
 		tb.setSize(width/4, height/2);
 		tb.setLocation(0, 60);
 		tb.setAlwaysOnTop(true);
 		tb.setVisible(true);
-		//		tb.add(button);
 
 
 		//Create a JPanel for the frame in which the checkboxes are displayed
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1,2000));
-//		panel.setSize(300, 900);
+
 		//An ArrayList containing all matches in the graph
 		List<Match> matches = graph.matches;
 
@@ -96,11 +81,9 @@ public class InternalFrame implements TreeSelectionListener {
 			root.add(instructionNode);
 			//and children to the instruction nodes
 			addMatchNodes(instructionNode, matches);
-
-
 		}
 
-		tree = new JTree(root);
+		tree = new CheckBoxTree(root);
 		tree.setRootVisible(true);
 
 		//		makes a scrollpane which displays the content of the tree
@@ -114,6 +97,47 @@ public class InternalFrame implements TreeSelectionListener {
 		//Listen for when the selection changes.
 		tree.addTreeSelectionListener(this);
 		panel.add(treeView);
+
+		tb.add(panel);
+
+	}
+
+
+	//	add children to the instruction nodes
+	public void addMatchNodes(DefaultMutableTreeNode parent, List<Match> matches){
+		//search for all the matches that have the same instruction id as the parent node
+		//they will be the leaves of the trees
+		for(Match m : matches){
+			int instrId = m.getInstructionId();
+			String name = "Instr-id : " + String.valueOf(instrId);
+			if(name.equals(parent.getUserObject())){
+
+				int matchId = m.getMatchId();
+				DefaultMutableTreeNode matchLeaf = new DefaultMutableTreeNode("Match-id : " + String.valueOf(matchId));
+				parent.add(matchLeaf);
+
+			}
+		}
+	}
+
+	@Override
+	public void valueChanged(TreeSelectionEvent e) {
+
+		//Returns the last path element of the selection.
+		//This method is useful only when the selection model allows a single selection.
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+				tree.getLastSelectedPathComponent();
+
+		if (node == null) return;
+
+	}
+
+
+
+
+	public void SpikOchLotta(JFrame frame, Graphiel graph, DefaultView view){
+
+		List<Match> matches = graph.matches;
 
 
 		//Loop through every match in the graph
@@ -171,56 +195,10 @@ public class InternalFrame implements TreeSelectionListener {
 			//			panel.add(cb);
 			//		panel.add(treeView);
 
+
 		}
 
-		//Add the panel to the toolbar frame
-		tb.add(panel);
-//		tb.getContentPane().add(comp, constraints);
-
 	}
-
-	//	add children to the instruction nodes
-	public void addMatchNodes(DefaultMutableTreeNode parent, List<Match> matches){
-		//search for all the matches that have the same instruction id as the parent node
-		//they will be the leaves of the trees
-		for(Match m : matches){
-			int instrId = m.getInstructionId();
-			String name = "Instr-id : " + String.valueOf(instrId);
-			if(name.equals(parent.getUserObject())){
-
-				int matchId = m.getMatchId();
-				DefaultMutableTreeNode matchLeaf = new DefaultMutableTreeNode("Match-id : " + String.valueOf(matchId));
-				parent.add(matchLeaf);
-
-			}
-		}
-	}
-
-	@Override
-	public void valueChanged(TreeSelectionEvent e) {
-
-		//Returns the last path element of the selection.
-		//This method is useful only when the selection model allows a single selection.
-		DefaultMutableTreeNode node = (DefaultMutableTreeNode)
-				tree.getLastSelectedPathComponent();
-
-		if (node == null) return;
-
-	}
-
 
 
 }
-
-//	//	add children to the instruction nodes
-//	public void addM(DefaultMutableTreeNode parent, List<Match> matches){
-//
-//		for(Match m : matches){
-//			if(m.getMatchId() == (int) parent.getUserObject()){
-//				DefaultMutableTreeNode matchNode = new DefaultMutableTreeNode(m.getMatchId());
-//				parent.add(matchNode);
-//
-//			}
-//		}
-//
-//	}
