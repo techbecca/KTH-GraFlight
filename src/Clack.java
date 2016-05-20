499983b91083a6ff4aa5b418968aa009c1955c51
Charlotta Spik <spik@kth.se>
2016-05-20 11:25:55 +0200

    fixed minor formating errors

--- src/Clack.java
+++ src/Clack.java
@@ -1,7 +1,6 @@
 import org.graphstream.graph.Node;
 import org.graphstream.ui.graphicGraph.GraphicElement;
 import org.graphstream.ui.view.View;
-
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -16,10 +15,8 @@ public class Clack implements MouseListener{
 	private View view = null;
 	private Graphiel g = null;
 	private int matchIndex = 0;
-	
 	int m = 0;
 	GraphicElement previousElement = null;
-
 
 	/**
 	 * This is a constructor
@@ -41,41 +38,41 @@ public class Clack implements MouseListener{
 		//Get the element (in this case a node) at the position of the mouse click
 		GraphicElement curElement = view.findNodeOrSpriteAt(e.getX(), e.getY());
 		if (curElement==null)return;
-		
+
 		//check if we are clicking on another node 
-				if(!curElement.equals(previousElement)){
-					m = 0;
-				}
-		
+		if(!curElement.equals(previousElement)){
+			m = 0;
+		}
+
 		//Save the element as a node n, as the element we want clickable is a node
 		Node n = g.getNode(curElement.toString());
-		
+
 		previousElement = curElement;
 
 		//Get all matches related to that node
 		ArrayList <Match> filteredMatches = g.filterByNode(n);
 		int max = filteredMatches.size()-1;
-		
+
 		//loop through the filteredMatches 
-				for (int i = m; i <= max; i++) {
-					
-					//get each match id from each match in the list 
-					int currentmatch = filteredMatches.get(m).getMatchId();
-					
-					Application.getGraph().currentmatch = currentmatch;
-					
-					//break out of loop after each match
-					if(filteredMatches.get(i).getMatchId() == currentmatch)
-						break;
-				}
-				//set m to 0 if we have reached the end of the filteredMatch list
-				if(m == max+1)
-					m = 0;
-				
-				//otherwise increment m 
-				else{
-					m++;
-				}
+		for (int i = m; i <= max; i++) {
+
+			//get each match id from each match in the list 
+			int currentmatch = filteredMatches.get(m).getMatchId();
+
+			Application.getGraph().currentmatch = currentmatch;
+
+			//break out of loop after each match
+			if(filteredMatches.get(i).getMatchId() == currentmatch)
+				break;
+		}
+		//set m to 0 if we have reached the end of the filteredMatch list
+		if(m == max+1)
+			m = 0;
+
+		//otherwise increment m 
+		else{
+			m++;
+		}
 
 		//Check if there is already an attribute "selected"
 		if(!UImod.checkuiC(n, "selected") /*g.getNode(String.valueOf(n)).hasAttribute("ui.style")*/){
@@ -89,13 +86,12 @@ public class Clack implements MouseListener{
 
 		}
 		else{
-			
 			//If there is and matchindex is less than max, which is the max number of matches for the node
 			if(matchIndex < max){
 				//Increment matchindex
 				matchIndex++;
 			}
-			
+
 			//now we have iterated through all the matches of the current node
 			else{
 				Match lastMatch = filteredMatches.get(max);
@@ -118,7 +114,6 @@ public class Clack implements MouseListener{
 			if (resetNode.hasAttribute("ui.style")){
 				resetNode.setAttribute("ui.style", "fill-color: rgb(10, 137, 255);");
 			}
-
 		}
 
 		Match match = filteredMatches.get(matchIndex);
