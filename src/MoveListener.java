69cc2a010862be9ec0a9310c475109750a74a064
Charlotta Spik <spik@kth.se>
2016-05-20 11:41:25 +0200

    edited comments

--- src/MoveListener.java
+++ src/MoveListener.java
@@ -1,12 +1,13 @@
 import org.graphstream.graph.Node;
 import org.graphstream.ui.graphicGraph.GraphicElement;
 import org.graphstream.ui.view.View;
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 /**
- * Created by Rebecca on 2016-05-19.
+ * This class saves the x- and y-position changes of a node into a stack to keep track of it's position position changes
+ * and thereby be able to reset the node's position.  
+ * @since 2016-05-20
  */
 public class MoveListener implements MouseListener{
     private View view = null;
@@ -19,7 +20,7 @@ public class MoveListener implements MouseListener{
 
     /**
      * Finds the node under the mouse-click and adds it to the stack of changed nodes (nodeChanges)
-     * @param e
+     * @param e A mouse event
      */
     @Override
     public void mousePressed(MouseEvent e) {
@@ -35,19 +36,18 @@ public class MoveListener implements MouseListener{
     @Override
     /**
      * Checks whether or not the node moved. If it didn't move, remove it from the stack nodeChanges (since it didn't change)
-     * @param e
+     * @param e A mouse event
      */
     public void mouseReleased(MouseEvent e) {
         GraphicElement curElement = view.findNodeOrSpriteAt(e.getX(), e.getY());
         if (curElement==null) return;
         Node n = g.getNode(curElement.toString());
 
-        // Save info for last element moved
+        //Save info for last element moved
         LastMoved lastMoved = new LastMoved(n);
         if (!Application.getNodeChanges().empty() && Application.getNodeChanges().peek().equals(lastMoved)){
             Application.getNodeChanges().pop();
         }
-
     }
 
     @Override
